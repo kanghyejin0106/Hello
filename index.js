6e76e7bc2319fc96e66f3ab14e6a86ce48eafe0f
Kang Hye Jin <34128604+kanghyejin0106@users.noreply.github.com>
2022-07-29 11:09:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var os = require('os');
 var port = 8080;
 
 http.createServer( (req, res) => {
-  console.log('Requested: ' + req.url);
-  res.writeHead(200, { 'Content-Type':'text/html; charset=utf-8' });
-  res.write('<h1>docker 예제1</h1>');
-  res.write('<h2>Hello node!</h2>');
-  res.end();
+    console.log('Requested: ' + req.url);
+    res.writeHead(200, { 'Content-Type':'text/html; charset=utf-8' });
+    res.write('<h1>docker 예제1</h1>');
+    res.write('<h2>Hello node!</h2>');
+    res.end();
 }).listen(port, () => {
-  console.log('Listen ... ' + os.hostname()+':'+port);
+    console.log('Listen ... ' + os.hostname()+':'+port);
 });
